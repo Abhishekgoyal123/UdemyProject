9ebe687c9d4d41c63ad33e2c7784f91797889a60
Abhishek Goyal <abhishek.goyal@coditas.com>
2022-11-08 12:02:43 +0530

    Add project files.

---
+++ WebApplication2/Controllers/RoleBasedAuthenticationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+//using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class RoleBasedAuthenticationController : Controller
+    {
+        [Authorize(Roles ="trainer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+       
+    }
+}
