1bd7b5c2e17cba4328905a88595e1c59a8068107
Abhishek Goyal <abhishek.goyal@coditas.com>
2022-11-21 16:15:07 +0530

    Login/Signup 21/11

---
+++ Udemy_Project/Controllers/AccountController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Udemy_Project.Models;
+using Udemy_Project;
+
+namespace Udemy_Project.Controllers
+{
+    public class AccountController : Controller
+    {
+        UdemyEntities context = new UdemyEntities();
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Login(User model, Role role)
+        {
+          
+            string localusername = model.UserName;
+            model.UserPassword = PasswordEncrypt.Encrypt(model.UserPassword);
+            bool isvalid = context.Users.Any(x => x.UserName == model.UserName && x.UserPassword == model.UserPassword);
+
+            if (isvalid)
+            {
+
+                var roleCheck = (from user in context.Users
+                           join rolemapping in context.RoleMappings on user.UserId equals rolemapping.UserId
+                           join roles in context.Roles on rolemapping.RoleId equals roles.RoleId
+                           where user.UserName == localusername
+                           select roles.RoleName).First();
+
+                if (roleCheck == "Trainer")
+                {
+                    return RedirectToAction("Index", "Trainer");
+                }
+
+                else if(roleCheck == "User")
+                {
+                    return RedirectToAction("Index", "User");
+                }
+
+               else
+                {
+                    return RedirectToAction("Index", "Admin");
+                }
+            }
+            else
+                return View("Error");
+            
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User model,Role role)
+        {
+            
+            model.UserPassword = PasswordEncrypt.Encrypt(model.UserPassword);
+
+            // role.user_id = model.id;
+            context.Users.Add(model);
+            context.SaveChanges();
+            //role.user_id = model.id;
+            //context.UserRoles.Add(role);
+            //context.SaveChanges();
+
+            return RedirectToAction("login");
+        }
+    }
+}
---
+++ Udemy_Project/PasswordEncrypt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using System.Security.Cryptography;
+using System.Text;
+
+namespace Udemy_Project
+{
+    public class PasswordEncrypt
+    {
+        public static string Encrypt(string Password)
+        {
+           // return Convert.ToBase64String(System.Security.Cryptography.SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(Password)));
+            return Convert.ToBase64String(System.Security.Cryptography.MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(Password)));
+        }
+    }
+}
